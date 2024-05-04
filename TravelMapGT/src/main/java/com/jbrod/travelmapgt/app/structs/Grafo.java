2d8e1b6e0e78b9f6a3af6a8ae7a4c1b8f6ab70a7
Jbrod5 <jorgebravo202131782@cunoc.edu.gt>
2024-05-04 01:32:57 -0600

    instanciacion de nodos

--- TravelMapGT/src/main/java/com/jbrod/travelmapgt/app/structs/Grafo.java
+++ TravelMapGT/src/main/java/com/jbrod/travelmapgt/app/structs/Grafo.java
@@ -35,6 +35,68 @@ public class Grafo {
     
     
     
+    /**
+     * Genera dos nodos (origen y destino, si no se encuentran ya) y luego los conecta entre si con su informacion.
+     * @param origen: String con el nombre de la locacion de origen. 
+     * @param destino: String con el nombre de la locacion de destino.
+     * 
+     * @param tiempo_vehiculo: int con el tiempo de la distancia en vehiculo. 
+     * @param tiempo_pie: int con el tiempo de la distancia a pie. 
+     * 
+     * @param consumo_gas: int con el consumo de gasolina para llegar al destino. 
+     * @param desgaste_persona: int con el desgaste de la persona para llegar al destino.
+     * 
+     * @param distancia: int con la distancia entre las dos locaciones.
+     **/
+    public void generarPar(String origen, String destino, 
+            int tiempo_vehiculo, int tiempo_pie,
+            int consumo_gas, int desgaste_persona,
+            int distancia){
+        
+        
+        //1. Generar nodos origen y destino
+        agregarNodo(origen);
+        agregarNodo(destino);
+        
+        //2. Establecer adyacencia y pesos DIRIGIDOS (carro)
+        Nodo nodoOrigenDirigido = buscarNodoDirigido(origen);
+        Nodo nodoDestinoDirigido = buscarNodoDirigido(destino);
+        // - adyacencia
+        nodoOrigenDirigido.agregarAdyacente(nodoDestinoDirigido);
+        // - pesos
+        nodoOrigenDirigido.agregarValores(destino, tiempo_vehiculo, distancia, consumo_gas);
+        
+        
+        
+        //3. Establecer adyacencia y pesos NO DIRIGIDO (pie)
+        Nodo nodoA = buscarNodoNoDirigido(origen);
+        Nodo nodoB = buscarNodoNoDirigido(destino);
+        if(nodoA != null && nodoB != null){
+            // - adyacencia
+            nodoA.agregarAdyacente(nodoB);
+            nodoB.agregarAdyacente(nodoA);
+            // - pesos
+            nodoA.agregarValores(destino, tiempo_pie, distancia, desgaste_persona);
+            nodoB.agregarValores(origen, tiempo_pie, distancia, desgaste_persona);
+        }else{
+            if(nodoA == null){
+                System.out.println("El nodo " + origen + " no existe.");
+            }
+            if(nodoB == null){
+                System.out.println("El nodo " + destino + " no existe.");
+            }
+        }
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
     public void establecerAdyacencia(String origen, String destino){
         //1. Obtener los nodos origen y destino de la lista de dirigidos
         Nodo nodoOrigen = buscarNodoDirigido(origen);
